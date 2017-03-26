6c4d8b50482e72640b2995aed9943cc3782e452b
ksato <ksato@iastate.edu>
2017-03-27 00:48:06 +0900

    Learning Ruby

---
+++ RubyTutorial/Test_Basics.rb
@@ -0,0 +1,55 @@
+class Meeting
+  attr_accessor :names
+
+  def initialize(names="World")
+    @names = names
+  end
+
+  def hello
+    if @names.nil?
+      puts "..."
+    elsif @names.respond_to?("even")
+      puts "Hi #{@names}!"
+    else
+      puts "Hello #{@names}!"
+    end
+  end
+
+  def bye
+    if @names.nil?
+      puts "..."
+    else
+      puts "Bye #{@names}!"
+    end
+  end
+end
+
+if __FILE__ == $0
+  mg = Meeting.new
+  mg.hello
+  mg.bye
+
+  mg.names = "Keaton"
+  mg.hello
+  mg.bye
+
+  mg.names = nil
+  mg.hello
+  mg.bye
+end
+
+
+#What I found
+#There is no data type
+#No semi-colon
+#nil is like 0
+#names.nil? is like if(names == null)
+#initialize is called when object is created
+#attr_accesstor makes instant variables
+#object is created by something = className.new
+#@ is used for variables??
+#elsif is used instead of else if
+#for loop is challenging
+#No need to use i for iterating
+#@names.each do creates loop
+#|name| contains the value of each index
--- RubyTutorial/test.rb
+++ RubyTutorial/test.rb
@@ -1,69 +1,14 @@
-class Greeting
-  attr_accessor :names
+name = "Keisuke"
 
-  #initialize is like constructor
-  def initialize(names = "World")
-    @names = names
-  end
+puts name
+puts name.reverse
+puts name.length
 
-  def hello
-    if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("each") #each is included in names
-      #loop
-      @names.each do |name|
-        puts "Hi #{name}!"
-      end
-    else
-      puts "Hello #{@names}!"
-    end
-  end
-
-  def bye
-    if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("each")
-      @names.each do |name|
-        puts "Good bye #{name}!"
-      end
-    else
-      puts "See you #{@names}"
-    end
-  end
+hsh = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+hsh.each do |key,value|
+  print key, " is ", value, "\n"
 end
 
-if __FILE__ == $0
-  mg = Greeting.new #mg is an object
-  mg.hello
-  mg.bye
-
-  mg.names = "John"
-  mg.hello
-  mg.bye
-
-  #names can be any types
-  mg.names = ["Keagen","Pyush","Yi-fan"]
-  mg.hello
-  mg.bye
-
-  #nil
-  mg.names = nil
-  mg.hello
-  mg.bye
-
+(10...15).each do |n|
+  print n, ' '
 end
-
-#What I found
-#There is no data type
-#No semi-colon
-#nil is like 0
-  #names.nil? is like if(names == null)
-#initialize is called when object is created
-#attr_accesstor makes instant variables
-#object is created by something = className.new
-#@ is used for variables??
-#elsif is used instead of else if
-#for loop is challenging
-  #No need to use i for iterating
-  #@names.each do creates loop
-  #|name| contains the value of each index
